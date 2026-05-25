c1f1d0acd734a35eb308049c9cf0fb717389aabe
Borges <borgespaulo72@yahoo.com.br>
2026-05-25 18:05:38 -0300

    feat: deixando funcional

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Auth } from './pages/auth/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Auth
+    },
+    {
+        path: 'home',
+        loadComponent() {
+            return import ('./pages/home/home').then(m => m.Home)
+        },
+    }
+];
